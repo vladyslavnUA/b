a3f98016547dd1746192bac239e0e783424a9a23
vladyslavnUA <vladyslavn@yahoo.com>
2019-11-20 11:02:55 -0800

    a

---
+++ calc.py
@@ -0,0 +1,17 @@
+def calculate():
+    total = float(input("what is your bill total? (i.e. $22.45) $ "))
+    tip = float(input("what is the tip percentage (%)? "))
+    global calc
+    calc = (total * (tip / 100)) + total
+    print(f'your bill total after a {tip} % tip is ${calc}')
+    split()
+    return calc
+
+def split():
+    t = calc
+    ask = input("do you want to split the bill with anyone? type y or n\n > ")
+    if ask == 'y':
+        ask2 = int(input("how many people do you want to split the bill with?\n > "))
+        final = (t / ask2)
+        print("each person's cut is $", final)
+calculate()
